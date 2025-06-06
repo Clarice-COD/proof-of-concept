966f045094b5f8accdc32802edad8d2a8a8c3372
Clarice-COD <clariceboerlijst@gmail.com>
2025-06-06 21:13:28 +0200

    server klaargezet
    Ik heb de server opgestart, de database gekoppeld en de pagina's alvast aangemaakt in de map views. #6

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 console.log('Hier komt je server voor Sprint 12.')
+ 
+import express from 'express' // Externe bibliotheek
+import { Liquid } from 'liquidjs'; // Importeerd liquid uit de bibliotheek
+import 'dotenv/config' // Importeerd dotenv uit de bibliotheek, dotevn zorgt ervoor dat je de variabelen uit het env bestand lokaal kunt lezen
+console.log(process.env.API_KEY)
+
+const app = express() // Express zorgt voor een aanroep voor het starten van een nieuwe webserver, zodat je door middel van app een route kan aanmaken
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express()); 
+app.set('views', './views')
+
+app.get('/', async (req, res) => { // HTTP GET-verzoek
+
+    const qersResponse = await fetch ('https://the-sprint-api.onrender.com/people', {
+        headers: {
+            'X-API-Key':`${process.env.API_KEY}`
+        }
+    });
+
+    const data = await qersResponse.json();
+    console.log(data)
+    res.render('index.liquid', { qers:data}); 
+});
+
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+ 
